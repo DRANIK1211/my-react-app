50882b3c971b42e0bca55cb1169f00d3ecc1cd09
DRANIK1211 <andreiduzhinin@yandex.ru>
2025-03-23 19:03:10 +0700

    start project

--- src/App.js
+++ src/App.js
@@ -2,30 +2,17 @@ import React, { useState } from 'react';
 
 function App() {
 
-  const [count, setCount] = useState(0)
   
-  function setInc(){
-    setCount(count+1)
-  }
-  function setDec(){
-    if(count > 0){
-      setCount(count-1)
-    }
-    
-  }
 
   return (
     <div className="App">
       
-      <h1>
-        {count}
-      </h1>
-      <br/>
-      <div>
-        <button onClick={setInc}>++</button>
-        <button onClick={setDec}>--</button>
+      <div class="Main">
+        <h1>Тестовое приложение</h1>
+        <img src="{{ url_for('static', filename='bot.png') }}" alt="123"/>
+        <p></p>
+        <button class="btn">Кнопка</button>
       </div>
-      <br/>
 
     </div>
   );
