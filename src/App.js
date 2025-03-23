b8fd0ba503f3868c3d0ef37a580d743c5133fdf9
DRANIK1211 <andreiduzhinin@yandex.ru>
2025-03-23 19:36:10 +0700

    add css

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import "./main.css"
 
 function App() {
 
@@ -7,11 +8,11 @@ function App() {
   return (
     <div className="App">
       
-      <div class="Main">
+      <div className="Main">
         <h1>Тестовое приложение</h1>
-        <img src="{{ url_for('static', filename='bot.png') }}" alt="123"/>
+        <img src="{{ url_for('static', filename='bot.png') }}" alt="123" />
         <p></p>
-        <button class="btn">Кнопка</button>
+        <button className="btn">Кнопка</button>
       </div>
 
     </div>
