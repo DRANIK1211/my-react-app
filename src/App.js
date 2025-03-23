7ba64faa11335aaefdcdf50c787c65188ef6d08f
DRANIK1211 <andreiduzhinin@yandex.ru>
2025-03-23 21:53:55 +0700

    delete error

--- src/App.js
+++ src/App.js
@@ -1,19 +1,20 @@
-import React, { useState } from 'react';
+import React from 'react';
 import "./main.css";
 import UserIdComponent from './UserIdComponent'; 
 
 function App() {  
 
   return (
+
     <div className="App">
       
       <div className='content'>
-        <div className='text'>Номер реестра</div>
+        <div className='text'>Номер реестра:</div>
         <div className='inputText'>
           <input type='text' id="inputText" />
         </div>
 
-        <div className='text'>Пароль</div>
+        <div className='text'>Пароль:</div>
         <div className='inputText'>
           <input type='text' id="inputPass" />
         </div>
