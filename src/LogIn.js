e234ea82f7fc3f9b052a93ded4c70ac7f03788ba
DRANIK1211 <andreiduzhinin@yandex.ru>
2025-03-25 16:29:51 +0700

    add form

--- src/LogIn.js
+++ src/LogIn.js
@@ -1,15 +1,47 @@
-import React from "react";
+import React, { useState } from "react";
 
 const LogIn = () => {
+
+    const [formData, setFormData] = useState({
+        number: '',
+        code: '',
+    });
+
+    const handleChange = (e) => {
+        const { name, value } = e.target;
+        setFormData({
+            ...formData,
+            [name]: value,
+        });
+    };
+
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        console.log('Submitted Data:', formData);
+        // Здесь вы можете отправить данные на сервер или выполнить другие действия
+        
+    };
+
     return (
         <div className='content'>
-            <div className='text'>Номер реестра:</div>
-            <div className='inputText'>
-                <input type='text' id="inputText" />
-            </div>
+            <form onSubmit={handleSubmit}>
+                <div className='text'>Номер реестра:</div>
+                <div className='inputText'>
+                    <input type='text' id="number" name="number" value={formData.name} onChange={handleChange} />
+                </div>
 
-            <div className='login' >Войти</div>
-        
+                <div className='text'>Код регистрации:</div>
+                <div className='inputText'>
+                    <input type='text' id="code" name="code" value={formData.code} onChange={handleChange} />
+                </div>
+
+                <div className='login' type="submit" >Войти</div>
+            </form>
+            
+            <p>
+                {formData}
+            </p>
         
         </div>
     )
