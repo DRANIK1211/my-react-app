1c478c11c42265f7ae1a33ea37d9c0f357413300
DRANIK1211 <andreiduzhinin@yandex.ru>
2025-03-27 17:17:52 +0700

    asd

--- src/MainScreen.js
+++ src/MainScreen.js
@@ -3,7 +3,7 @@ import React from "react";
 const MainScreen = ({user}) => {
     console.log(user)
     return(
-        <div className="content">
+        <div className="content main">
             <div className="main-text">Ваши меда</div>
             <div className="main-text-znak">
                 <div className="p">Наименование</div>
