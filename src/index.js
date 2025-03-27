f24c7cca59e8e1b536d159ac91c0fd2c2d8bd5d3
DRANIK1211 <andreiduzhinin@yandex.ru>
2025-03-27 16:27:14 +0700

    delete error

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import "./main.css"
+import "./style/main.css"
+import "./style/index.css"
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
