ab8ca7ab3594c41620dd1222d41bc56986e18d74
DRANIK1211 <andreiduzhinin@yandex.ru>
2025-03-29 15:24:26 +0700

    search error 5

--- src/MainScreen.js
+++ src/MainScreen.js
@@ -35,6 +35,19 @@ const MainScreen = ({user}) => {
 
     const del = (num)=>{
         alert("num: " + num)
+        const userService = UserService("https://andreydrughinin.pythonanywhere.com");
+        userService.delMed(num)
+        .then(
+            ()=>{
+                alert("ok")
+                let new_med = med.filter(item => item.number !== num)
+                setMed(new_med)
+            } 
+        ).catch(
+            (error)=>{
+                alert(error)
+            }
+        )
     }
 
     let medList = <div></div>
