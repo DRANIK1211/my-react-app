5e42d1da3cfd5ccf5900aec0e14e4d601eb73ce9
DRANIK1211 <andreiduzhinin@yandex.ru>
2025-03-29 15:04:44 +0700

    try 4

--- src/MainScreen.js
+++ src/MainScreen.js
@@ -35,12 +35,12 @@ const MainScreen = ({user}) => {
             return (
                 <div className="med">
                     <div className="med-component">
-                        <div className="component-name">{med.name}</div>
-                        <div className="component-count">{med.amount}</div>
+                        <div className="component-name">{i.name}</div>
+                        <div className="component-count">{i.amount}</div>
                     </div>
                     <div className="med-btn">
                         <div className="btn-red">Редактировать</div>
-                        <div className="btn-del" onClick={() => del(med.number)}>Удалить</div>
+                        <div className="btn-del" onClick={() => del(i.number)}>Удалить</div>
                     </div>
                 </div>
             )
