7874d7a4d7c359a2680a64e131859e222ff4af11
DRANIK1211 <andreiduzhinin@yandex.ru>
2025-03-29 12:37:24 +0700

    try 6

--- src/med-component.js
+++ src/med-component.js
@@ -4,8 +4,8 @@ const medComponent = (med) => {
     return (
         <div className="med">
             <div className="med-component">
-                <div className="component-name">med.name</div>
-                <div className="component-count">med.amount</div>
+                <div className="component-name">{med.name}</div>
+                <div className="component-count">{med.amount}</div>
             </div>
             <div className="med-btn">
                 <div className="btn-red">Редактировать</div>
