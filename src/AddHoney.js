22e454f0a2799379c53fa3562efb4d16e85996ac
DRANIK1211 <andreiduzhinin@yandex.ru>
2025-04-05 21:13:29 +0700

    добавил остальные части формы

--- src/AddHoney.js
+++ src/AddHoney.js
@@ -5,12 +5,19 @@ const AddHoney = ({ ok }) => {
 
     const med_list = ["Гречишный", "Темное разнотравье с гречихой", "Горный", "Дягилевый", "Рапсовый", "Светлое разнотравье с посевными"]
     const [selectedHoney, setSelectedHoney] = useState('--Не выбрано--');
-    const handleChange = (event) => {
+    const handleChangeName = (event) => {
         setSelectedHoney(event.target.value);
     };
     const handleSubmit = () => {
         ok()
     }
+    const handleInputChange = (e) => {
+        const { name, value, type, checked } = e.target;
+        setFormData(prev => ({
+          ...prev,
+          [name]: type === 'checkbox' ? checked : value
+        }));
+      };
 
     return (
         <div className="wrapper">
@@ -19,7 +26,7 @@ const AddHoney = ({ ok }) => {
                 <div className='input-block'>
                     <select
                         value={selectedHoney}
-                        onChange={handleChange}
+                        onChange={handleChangeName}
                         className="select-name"
                     >
                         {
@@ -27,6 +34,32 @@ const AddHoney = ({ ok }) => {
                         }
                     </select>
                 </div>
+                <div className="block">
+                    <div className="name-block">Количество (в килограммах)</div>
+                    <div className="input-block">
+                        <input type="number" name="amount" value={formData.amount} onChange={handleInputChange} required />
+                    </div>
+                </div>
+                <div className="block">
+                    <div className="name-block">Количество с меркурием (в килограммах)</div>
+                    <div className="input-block">
+                        <input type="number" name="amountMerc" value={formData.amountMerc} onChange={handleInputChange} required />
+                    </div>
+                </div>
+                <div className="block">
+                    <div className="name-block">Местонахождение мёда (Область, город)</div>
+                    <div className="input-block">
+                        <input type="text" name="address" value={formData.address} onChange={handleInputChange} required />
+                    </div>
+                </div>
+                
+                <label className="check-block">
+                    <input className="check" type="checkbox" name='analysis' checked={formData.analysis} onChange={handleInputChange} />
+                    <span className="custom-check"></span>
+                    <div className="check-name">Есть пыльцевой анализ</div>
+                </label>
+
+                <button type='submit' className="btn-form-red">Добавить</button>
             </form>
         </div>
     )        
